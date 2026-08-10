4776dcb0d0503a886b7cef3e77d6b8b79d73884a
deveshsahu16 <deveshsahu16@yahoo.com>
2026-08-10 22:42:54 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    fullName: {
+        type: String,
+        required: true, 
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+    coverImage: {
+        type: String,
+    },
+    watchHistory: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+    }],
+    refreshToken: {
+        type: String,
+    }
+},{ timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function() {
+    jwt.sign(
+        { 
+            id: this._id, 
+            username: this.username,
+            email: this.email,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { 
+            expiresIn: process.env.Access_TOKEN_EXPIRES_IN
+        },
+    )
+}
+userSchema.methods.generateRefreshToken = function() {
+    jwt.sign(
+        { 
+            id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { 
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+        },
+    )
+}
+
+export const User = new mongoose.model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const videoSchema = new mongoose.Schema({
+    videFile: {
+        type: String,
+        required: true, 
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+}, { timestamps: true });
+
+export const Video = new mongoose.model("Video", videoSchema);
